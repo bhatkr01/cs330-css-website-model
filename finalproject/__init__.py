818943e833364a9b031299af110c8ad9eb32f2f6
Kritib Bhattarai <bhatkr01@kritibs-mbp.lc.luther.edu>
2021-05-06 08:35:30 -0500

    c6

--- finalproject/__init__.py
+++ finalproject/__init__.py
@@ -5,7 +5,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_login import LoginManager
-import main, auth
 db=SQLAlchemy()
 mm=Marshmallow()
 
